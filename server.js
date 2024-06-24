5ca72aba424a6d7497772ad02e367f55943429b2
M-Ridha <82166718+M-Ridha@users.noreply.github.com>
2024-06-24 12:47:04 +0100

    fix port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 const express = require('express')
 const app = express()
-const port = 4000
+const port = 8080
 const checktime = (req, res, next) => {
     const date = new Date()
     const days = date.getDay()
